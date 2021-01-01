7575ff177f6f71fc9159dcb5cb4318d1718c8e8d
Jason <xiongjason2002@gmail.com>
2021-01-01 15:18:24 -0500

    implement cohesion, refactor some variables

--- boid.js
+++ boid.js
@@ -5,33 +5,68 @@ class Boid {
         this.velocity.setMag(random(2, 4));
         this.acceleration = createVector();
         this.radians = 0;
+        this.maxForce = 0.2;
+        this.maxSpeed = 4;
+        this.radius = 50;
     }    
 
+    flocking(boids){
+        this.align(boids);
+        this.cohesion(boids);
+    }
     align(boids){
         let desired = createVector();
-        let radius = 50;
         let total= 0;
         for(let boid of boids){
             let d = dist(this.position.x, this.position.y, boid.position.x,boid.position.y);
             
-            if(boid != this && d < radius){
+            if(boid != this && d < this.radius){
                 desired.add(boid.velocity);
                 total ++;
             }
         }
         if(total > 0){
             desired.div(total);
+            desired.setMag(this.maxSpeed);
             desired.sub(this.velocity);
+            desired.limit(this.maxForce);
+
         }else{
             desired = 0;
         }
-        this.acceleration = desired;
+        this.acceleration.add(desired);
     }
 
+    cohesion(boids){
+        let desired = createVector();
+        
+        let total= 0;
+        for(let boid of boids){
+            let d = dist(this.position.x, this.position.y, boid.position.x,boid.position.y);
+            
+            if(boid != this && d < this.radius){
+                desired.add(boid.position);
+                total ++;
+            }
+        }
+        if(total > 0){
+            desired.div(total);
+            desired.sub(this.position);
+            desired.setMag(this.maxSpeed);
+            desired.sub(this.velocity);
+            desired.limit(this.maxForce);
+
+        }else{
+            desired = 0;
+        }
+        this.acceleration.add(desired);
+    }
 
     update(){
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        this.acceleration.mult(0);
         if(this.position.x > width){
             this.position.x = 0;
         }
@@ -46,9 +81,9 @@ class Boid {
         }
     }
     detection(){
-        fill(255, 255, 255, 20);
+        fill(255, 255, 255, 5);
         stroke(0, 0, 0, 0);
-        circle(this.position.x, this.position.y, 100);
+        circle(this.position.x, this.position.y, 2*this.radius);
     }
     show(){
         push();
